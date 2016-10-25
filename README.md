a809993777e8e60f230f177e16ad564e450653e9
EdamLee <edam.lqq@gmail.com>
2016-10-25 11:58:16 -0700

    setting up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ARA
 APP 2 Project
+
+
+
+Build and Run `cradle run`
+
+Test on localhost:4567/hello
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
