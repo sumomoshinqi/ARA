bb52b5fa555a8908ef6c50ad3ed2662a24a29a82
EdamLee <edam.lqq@gmail.com>
2016-11-05 16:44:07 -0700

    CRUD for Cars

--- src/main/java/com/ARA/CarDAO.java
+++ src/main/java/com/ARA/CarDAO.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import spark.Request;
 
-
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.ArrayList;
@@ -16,7 +15,7 @@ import org.mongodb.morphia.dao.BasicDAO;
 
 
 /**
- * implementation of CarDAO
+ * implementation of Car's Data Access Object
  * @author Edam
  *
  */
