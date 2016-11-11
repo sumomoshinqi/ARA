095cc74d61e4a21dfd5bcec4c192556c12abb2e7
Ruby Wang <rubywang@PieStarMac.local>
2016-11-11 00:01:14 -0800

    car valide RideType update

--- src/main/java/com/ARA/util/Validate.java
+++ src/main/java/com/ARA/util/Validate.java
@@ -5,6 +5,7 @@ import com.ARA.module.Driver;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Validate data type
@@ -15,13 +16,29 @@ public class Validate {
                 || car.getColor().length() > 10 || car.getColor().length() > 20)
             return false;
         List<String> rideTypes = car.getValidRideTypes();
+        List<String> validRideTypes = Arrays.asList("ECONOMY", "PREMIUM", "EXECUTIVE");
         for (String type : rideTypes) {
-            if (!rideTypes.contains(type))
+            if (!validRideTypes.contains(type))
                 return false;
         }
         return true;
     }
     public static boolean validDriver (Driver driver) {
+        HashMap<String, Integer> driverProperty = new HashMap<String, Integer>();
+        driverProperty.put("firstName", 50);
+        driverProperty.put("lastName", 50);
+        driverProperty.put("emailAddress", 50);
+        driverProperty.put("password", 50);
+        driverProperty.put("addressLine1", 50);
+        driverProperty.put("addressLine2", 50);
+        driverProperty.put("city", 50);
+        driverProperty.put("state", 50);
+        driverProperty.put("zip", 50);
+        driverProperty.put("city", 50);
+        driverProperty.put("phoneNumber", 50);
+        driverProperty.put("drivingLicense", 50);
+        driverProperty.put("drivingLicense", 2);
+
         if (driver.getFirstName().length() > 50 || driver.getLastName().length() > 50 || driver.getEmailAddress().length() > 50
                 || driver.getPassword().length() > 20 || driver.getPassword().length() < 8 || driver.getAddressLine1().length() > 100 
                 || driver.getCity().length() > 50 || driver.getState().length() != 2 || driver.getZip().length() != 5 
